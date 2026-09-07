88f724bab055e793c954c9b1abd6df8bfed3916e
amcdev1 <241708697+amcdev1@users.noreply.github.com>
2026-09-06 23:49:58 -0700

    point Homebrew formula at public distribution assets

--- Formula/gantry.rb
+++ Formula/gantry.rb
@@ -5,13 +5,13 @@ class Gantry < Formula
   license "Apache-2.0"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/trygantry/gantry/releases/download/v#{version}/gantry-macos-aarch64"
+    url "https://github.com/trygantry/gantry-distribution/releases/download/v#{version}/gantry-macos-aarch64"
     sha256 "5e866d956920ed8824103d74075e3489971ebb4f5c6d87aaa4102f59b9cf0466"
   elsif OS.mac?
-    url "https://github.com/trygantry/gantry/releases/download/v#{version}/gantry-macos-x86_64"
+    url "https://github.com/trygantry/gantry-distribution/releases/download/v#{version}/gantry-macos-x86_64"
     sha256 "53cd709ad2135a853264f6e905fd794acd52d41323470751a3f9756749ffe57f"
   else
-    url "https://github.com/trygantry/gantry/releases/download/v#{version}/gantry-linux-x86_64"
+    url "https://github.com/trygantry/gantry-distribution/releases/download/v#{version}/gantry-linux-x86_64"
     sha256 "fae04564a7da30b8e62eb3478be50ed77c7ae5a60f9cdfe52714d2c16e334448"
   end
 
